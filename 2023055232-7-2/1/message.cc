e0fc595307d45bb8d0d9c83d8ee592eb8452a2a9
강민규 <sonic02417@naver.com>
2023-11-08 18:54:28 +0900

    csp-assignment

---
+++ 2023055232-7-2/1/message.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "message.h"
+
+using namespace std;
+
+MessageBook::MessageBook(){
+
+}
+MessageBook::~MessageBook(){
+
+}
+
+void MessageBook::AddMessage(int number, const std::string& message){
+messages_.insert(make_pair(num,message))
+   
+}
+void MessageBook::DeleteMessage(int number){
+
+}
+std::vector<int> MessageBook::GetNumbers(){
+    int inputnum;
+    
+    
+}
+const std::string& MessageBook::GetMessage(int number){
+    
+
+}
